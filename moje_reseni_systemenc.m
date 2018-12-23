b95e8290ef147ff0b39b46972d096cadadfe20aa
Michal Pospíšil <xpospi95@stud.fit.vutbr.cz>
2018-12-23 19:41:14 +0100

    Hotovy MATLAB kod

--- moje_reseni_systemenc.m
+++ moje_reseni_systemenc.m
@@ -128,8 +128,8 @@ hold off
 %% Úloha 7
 disp("Uloha 7");
 % Rátanie poètu nezhôd
-errs = sum(xor(symbols, symbols2));
-shiftedErrs = sum(xor(symbols, symbols3));
+errs = sum(xor(symbols(1:end-1), symbols2));
+shiftedErrs = sum(xor(symbols(1:end-1), symbols3));
     
 disp("Chyba pri dekodovani z filtrovanho signalu [%]:");
 disp(errs/(N/16)*100);
@@ -182,4 +182,83 @@ disp(cor10(52));
 disp('R[16]:');
 disp(cor10(67));
 
+%% Úloha 12
+disp('Uloha 12');
 
+function [h,p,r] = hist2(y1,y2,x); 
+% function [h,p,r] = hist2(y1,y2,x); 
+%
+% y1 and y2 are column vectors with data (must have same dimension)
+% x  are centers of histogram bins for the 2 coordinates. 
+%    should be equally spaced !!!
+% h is the 2-D histogram (y1 distrib in cols, y2 in rows)
+% 
+% p is the estimate of 2-D prob. dens. function
+%
+% r is he autocorrelation coefficient computed using the theoretical 
+% formula (5-8 in Sebesta) 
+%
+% ... histogram computation is not very optimized ... 
+
+L = length(x); 
+N = length(y1); 
+
+% alloc for hist 
+h = zeros(L,L); out = 0; % counter of bad samples...
+
+% make a BIG matrix with all values of x ... will actually do something 
+% like vector quantization. 
+% first make col vector out of x, then repeat it N times. 
+xcol = x(:); bigx = repmat(xcol,1,N); 
+
+% make rows out of y1 , y2 and do the 'quantization' 
+yr = y1(:)'; bigy = repmat(yr,L,1);
+[dummy,ind1] = min(abs(bigy - bigx)); 
+yr = y2(:)'; bigy = repmat(yr,L,1);
+[dummy,ind2] = min(abs(bigy - bigx)); 
+
+% now just go through the indices and increment respective cases in h
+for ii=1:N,
+  d1 = ind1(ii);   d2 = ind2(ii); 
+  h(d1,d2) = h(d1,d2) + 1; 
+endfor
+
+%%% prob dens: will have to normalize
+surf = (x(2) - x(1))^2; % surface of one tile
+p = h / N / surf;  
+
+%%%% autocor coeff 
+% make col vector out of x and clone it L times. 
+x = x(:); X1 = repmat(x,1,L);
+% make row vector out of x and clone it L times. 
+x = x'; X2 = repmat(x,L,1); 
+% now put it together, don't forget to multipl by tile surface
+r = sum(sum (X1 .* X2 .* p)) * surf;
+
+%%% check ... 
+check = sum(sum (p)) * surf; 
+disp(['hist2: check -- 2d integral should be 1 and is ' num2str(check)]); 
+endfunction
+
+% Posun o 1 vlavo 
+ss12 = s(2:N);
+
+% Orezanie s na rovnaky rozmer
+s12 = s(1:N-1);
+
+chlieviky = linspace(-1,1,25);
+
+[h12, p12, r12] = hist2(s12,ss12,chlieviky);
+
+figure('Name','Uloha 12');
+imagesc(chlieviky, chlieviky, p12);
+colorbar;
+
+%% Úloha 13
+disp('Uloha 13');
+disp("Dokaz urobila funkcia hist2 a je vytlaceny v predchadzajucej ulohe.");
+
+%% Úloha 14
+disp('Uloha 14');
+disp('R[1]:');
+disp(r12);
